a7fb0b22abd647360eea1803e1868204d2e668b6
Abilash kb <43920638+abilash1907@users.noreply.github.com>
2021-10-09 11:29:13 +0530

    left rotation of array

---
+++ Python/array-left-rotation.py
@@ -0,0 +1,24 @@
+def rotLeft(a, d):
+    # Write your code here
+    for i in range(d):
+        ele=a.pop(0)
+        a.append(ele)
+    return a
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    d = int(first_multiple_input[1])
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = rotLeft(a, d)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
