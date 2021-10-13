a736de0e4d860b2c9a6809323ba59da578aa04c1
Ayush khandelwal <ayushkhandelwal519@gmail.com>
2021-10-13 23:59:03 +0530

    functions using c++

---
+++ C++/ncr.cpp
@@ -0,0 +1,22 @@
+// calculation of nCr
+
+#include<iostream>
+using namespace std;
+
+int fact(int n)
+    
+{
+ int factorial =1;
+    for (int i = 2; i <= n; i++)
+    {
+        factorial= factorial*1;
+    }
+    return factorial;
+}
+int main(){
+    int n,r;
+    cin>>n>>r;
+    int ans = fact(n)/(fact(r)*fact(n-r));
+    cout<<ans<<endl;
+    return 0;
+}
